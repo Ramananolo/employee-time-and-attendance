c3e077abb706945f89695224e1cb8b0e844764d0
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-24 12:56:04 +0300

    feat: incomplete employee-time-and-attendance

---
+++ src/main/java/employee/attendance/employeeTimeAndAttendance/EmployeeTimeAndAttendanceApplication.java
@@ -0,0 +1,13 @@
+package employee.attendance.employeeTimeAndAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTimeAndAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTimeAndAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/employee/attendance/employeeTimeAndAttendance/EmployeeTimeAndAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package employee.attendance.employeeTimeAndAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTimeAndAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
