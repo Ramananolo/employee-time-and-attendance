cc0a30eb881115123e044ef2fb09f9fa24590ac5
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-24 13:31:09 +0300

    feat: category class and his atribute

--- src/main/java/employee/attendance/employeeTimeAndAttendance/entity/Category.java
+++ src/main/java/employee/attendance/employeeTimeAndAttendance/entity/Category.java
@@ -1,2 +1,16 @@
-package employee.attendance.employeeTimeAndAttendance.entity;public class Category {
+package employee.attendance.employeeTimeAndAttendance.entity;
+
+import employee.attendance.employeeTimeAndAttendance.entity.enums.ConditionOfWorking;
+import employee.attendance.employeeTimeAndAttendance.entity.enums.ListCategory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Category {
+    private final ListCategory nom;
+    private int hourNormalByWeek;
+    private double salaryPerWeek;
+    private double indemnity;
+    private ConditionOfWorking workingConditions;
 }
