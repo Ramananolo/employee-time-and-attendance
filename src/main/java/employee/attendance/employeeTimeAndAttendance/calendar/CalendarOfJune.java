f69ab65d3089bb954332ec2b892d1f08d2333acd
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-06-24 13:36:29 +0300

    feat: CalendarOfJune and add all ironDay in Ironday List

--- src/main/java/employee/attendance/employeeTimeAndAttendance/calendar/CalendarOfJune.java
+++ src/main/java/employee/attendance/employeeTimeAndAttendance/calendar/CalendarOfJune.java
@@ -1,2 +1,31 @@
-package employee.attendance.employeeTimeAndAttendance.calendar;public class CalendarOfJune {
+package employee.attendance.employeeTimeAndAttendance.calendar;
+
+import employee.attendance.employeeTimeAndAttendance.entity.Employee;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+@Getter
+@Setter
+public class CalendarOfJune {
+    private final Calendar calendarJune = new GregorianCalendar(2024, Calendar.JUNE, 1);
+    public List<Calendar> ironDay;
+    int numberDayOfWorkingPerWeek = Calendar.DAY_OF_WEEK;
+    Employee employee;
+
+    public CalendarOfJune() {
+        this.ironDay = new ArrayList<>();
+        Calendar date17 = (Calendar) calendarJune.clone();
+        Calendar date25 = (Calendar) calendarJune.clone();
+        Calendar date26 = (Calendar) calendarJune.clone();
+        date26.set(Calendar.DAY_OF_MONTH, 26);
+        date25.set(Calendar.DAY_OF_MONTH, 25);
+        date17.set(Calendar.DAY_OF_MONTH, 17);
+        this.ironDay.add(date17);
+        this.ironDay.add(date26);
+        this.ironDay.add(date25);
+    }
 }
